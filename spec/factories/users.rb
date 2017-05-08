a6fda9d4e34924e5a295012ac33c3fda7d95d9fc
Anton Fefilov <anton.fefilov@macrocoders.com>
2017-05-08 17:22:52 +0400

    add users

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    birth_date "2017-05-08"
+    bio "MyText"
+    avatar "MyString"
+  end
+end
