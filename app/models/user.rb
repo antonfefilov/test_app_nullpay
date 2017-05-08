9cadd4279a50144676f3f276ffcd40184db9b4e1
Anton Fefilov <anton.fefilov@macrocoders.com>
2017-05-08 17:42:03 +0400

    annotate User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  first_name :string
+#  last_name  :string
+#  birth_date :date
+#  bio        :text
+#  avatar     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  first_name :string
+#  last_name  :string
+#  birth_date :date
+#  bio        :text
+#  avatar     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :user do
     email "MyString"
